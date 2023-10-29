a68c543874fc962c58a38e709010f5b866bf62cc
Kai-Kuze <ssrnokai@gmail.com>
2023-10-30 01:52:46 +0900

    add getTitle.pl

---
+++ getTitle.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use Selenium::Chrome;
+
+my $driver = Selenium::Chrome->new(
+	binary => '/usr/local/bin/chromedriver',
+);
+
+print "Type a URL:";
+my $url = <STDIN>;
+chomp($url);
+while (!$url){
+	print "URL is empty. Please retry.:";
+	$url = <STDIN>;
+	chomp($url);
+}
+
+$driver->get($url);
+print $driver->get_title()."\n";
+$driver->shutdown_binary();
